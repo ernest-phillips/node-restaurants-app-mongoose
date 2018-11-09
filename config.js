8f1d7e2c17077a9d62c2dee8f3512de5788a41dc
ernest-phillips <ernestlphillips@gmail.com>
2018-11-09 06:48:28 -0800

    final commit

--- config.js
+++ config.js
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-"use strict";
-exports.DATABASE_URL =
-  process.env.DATABASE_URL || "mongodb://localhost/restaurants-app";
-exports.TEST_DATABASE_URL =
-  process.env.TEST_DATABASE_URL || "mongodb://localhost/test-restaurants-app";
-exports.PORT = process.env.PORT || 8080;
-=======
 'use strict';
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/restaurants-app';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-restaurants-app';
 exports.PORT = process.env.PORT || 8080;
->>>>>>> feature/with-tests
